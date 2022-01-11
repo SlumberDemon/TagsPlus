a94c604c23b6107dca4c99ad4bf65804f941a956
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-01-11 13:27:17 +0100

    added guild_delete_tag

--- src/extras/func.py
+++ src/extras/func.py
@@ -4,10 +4,14 @@ from typing import Union
 
 deta = Deta(os.getenv('DETA'))
 
-async def guild_push_object(guildId: int, item: Union[list, dict], key: str):
+async def guild_push_tag(guildId: int, item: Union[list, dict], key: str):
     db = deta.Base(f'Guild-{guildId}')
-    db.put({'item': item}, key)
+    db.insert({'item': item}, key)
 
-async def guild_get_object(guildId: int, key: str):
+async def guild_get_tag(guildId: int, key: str):
     db = deta.Base(f'Guild-{guildId}')
     return db.get(key)
+
+async def guild_delete_tag(guildID: int, key: str):
+    db = deta.Base(f'Guild-{guildID}')
+    return db.delete(key)
