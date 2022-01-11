e7f91ae4d068a7bd19b30813831f66a12653a6f5
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-01-11 17:25:51 +0100

    Test messages for tag command

--- src/cogs/tags.py
+++ src/cogs/tags.py
@@ -8,11 +8,19 @@ class Tags(commands.Cog):
 
     @commands.group(name='tag', invoke_without_command=True)
     async def tag(self, ctx):
-        await ctx.send('Placeholder')
+        await ctx.send('Main')
 
     @tag.command(name='create')
     async def create_tag(self, ctx):
-        await ctx.send('Placeholder')
+        await ctx.send('Main -> Create')
+
+    @tag.command(name='edit')
+    async def edit_tag(self, ctx):
+        await ctx.send('Main -> Edit')
+
+    @tag.command(name='delete')
+    async def delete_tag(self, ctx):
+        await ctx.send('Main -> Delete')
 
 def setup(bot):
     bot.add_cog(Tags(bot))
