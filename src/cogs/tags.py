780f96c769b3b986ae7fd1b6a3764f7e94930424
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-01-11 19:03:02 +0100

    ?tag raw

--- src/cogs/tags.py
+++ src/cogs/tags.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 from src.extras.func import *
 from src.extras.func import guild_create_tag
+from src.extras.func import guild_get_tag
 
 class Tags(commands.Cog):
 
@@ -28,6 +29,12 @@ class Tags(commands.Cog):
     @tag.command(name='show')
     async def show_tag(self, ctx):
         await ctx.send('Main -> Show')
+
+    @tag.command(name='raw')
+    async def raw_tag(self, ctx, tag):
+        data = await guild_get_tag(guildId = ctx.guild.id, key=f'{tag}')
+        embed = discord.Embed(title=f'Results for [{data}]', description=f'```py' f'\n{data}' f'\n```')
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Tags(bot))
