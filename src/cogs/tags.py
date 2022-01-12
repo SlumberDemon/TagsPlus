dc39970f62f3e5b89ef6b01bf80657a41c8bd4ab
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-01-12 16:56:01 +0100

    Added Tag not found

--- src/cogs/tags.py
+++ src/cogs/tags.py
@@ -29,14 +29,17 @@ class Tags(commands.Cog):
 
     @tag.command(name='edit')
     async def edit_tag(self, ctx, tag, *, content:str):
-        data = await guild_get_tag(guildId=ctx.guild.id, key=tag)
-        owner = data['item'][0]['owner']
         time = datetime.datetime.now() 
-        if f'{ctx.author.id}' == f'{owner}':
-            await guild_edit_tag(guildId=ctx.guild.id, item=[{"owner":f'{ctx.author.id}', "name":tag, "content":content, "created_at":f'{time.day}/{time.month}/{time.year}'}], key=tag)
-            await ctx.send(f'Tag {tag} successfully edited.')
-        else:
-            await ctx.send('You don\'t own this tag.', view=None)
+        try:
+            data = await guild_get_tag(guildId=ctx.guild.id, key=tag)
+            owner = data['item'][0]['owner']
+            if f'{ctx.author.id}' == f'{owner}':
+                await guild_edit_tag(guildId=ctx.guild.id, item=[{"owner":f'{ctx.author.id}', "name":tag, "content":content, "created_at":f'{time.day}/{time.month}/{time.year}'}], key=tag)
+                await ctx.send(f'Tag {tag} successfully edited.')
+            else:
+                await ctx.send('You don\'t own this tag.', view=None)
+        except:
+            await ctx.send('Tag not found.')
 
 
     @tag.command(name='delete')
