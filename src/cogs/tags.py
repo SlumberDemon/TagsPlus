9ff47c14e0bc13d428abbeb7c22a07a41d070db0
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-01-12 10:05:36 +0100

    changed layout

--- src/cogs/tags.py
+++ src/cogs/tags.py
@@ -42,9 +42,10 @@ class Tags(commands.Cog):
     async def info(self, ctx, tag):
         # try:
             info = await guild_get_tag(guildId=ctx.guild.id, key=tag)
-            em = discord.Embed(title=info['item'][0]['name'], description='Content ' + info['item'][0]['content'])
+            em = discord.Embed(title=info['item'][0]['name'], timestamp=info['item'][0]['created_at'])
+            em.add_field(name='Content', value='info['item'][0]['content']')
             em.add_field(name='Owner', value='<@' + info['item'][0]['owner'] + '>')
-            em.add_field(name='Created at', value=info['item'][0]['created_at'])
+            em.set_footer(text='Created at ')
             await ctx.send(embed=em)
 
 def setup(bot):
