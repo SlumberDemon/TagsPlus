f0bb0a28adca44f4d1a289f767c2d01317da31b2
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-01-12 19:01:27 +0100

    Added global func

--- src/extras/func.py
+++ src/extras/func.py
@@ -4,6 +4,8 @@ from typing import Union
 
 deta = Deta(os.getenv('DETA'))
 
+# Guild
+
 async def guild_create_tag(guildId: int, item: Union[list, dict], key: str):
     db = deta.Base(f'Guild-{guildId}')
     db.insert({'item': item}, key)
@@ -18,5 +20,23 @@ async def guild_get_tag(guildId: int, key: str):
 
 async def guild_delete_tag(guildId: int, key: str):
     db = deta.Base(f'Guild-{guildId}')
+    db.delete(key)
+
+# Global
+
+async def global_create_tag(item: Union[list, dict], key: str):
+    db = deta.Base(f'Global')
+    db.insert({'item': item}, key)
+
+async def global_edit_tag(item: Union[list, dict], key: str):
+    db = deta.Base(f'Global')
+    db.put({'item': item}, key)
+
+async def global_get_tag(key: str):
+    db = deta.Base(f'Global')
+    return db.get(key)
+
+async def global_delete_tag(key: str):
+    db = deta.Base(f'Global')
     db.delete(key)
 
