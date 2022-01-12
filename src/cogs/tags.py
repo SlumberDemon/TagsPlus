402cfa8bd6e80e5f83a3592e8bef57b697d8e23f
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-01-12 11:22:34 +0100

    updated created_at

--- src/cogs/tags.py
+++ src/cogs/tags.py
@@ -1,3 +1,4 @@
+from math import exp
 import discord, datetime 
 from discord.ext import commands
 from src.extras.func import *
@@ -14,7 +15,8 @@ class Tags(commands.Cog):
     @tag.command(name='create')
     async def create_tag(self, ctx, name, *, content:str):
         try:
-            await guild_create_tag(guildId=ctx.guild.id, item=[{"owner":f'{ctx.author.id}', "name":name, "content":content, "created_at":f'{datetime.datetime.utcnow()}'}], key=name)
+            time = datetime.datetime.now() 
+            await guild_create_tag(guildId=ctx.guild.id, item=[{"owner":f'{ctx.author.id}', "name":name, "content":content, "created_at":f'{time.day}/{time.year}/{time.month}'}], key=name)
             await ctx.send(f'Tag {name} successfully created.')
         except:
            await ctx.send('This tag already exists.')
@@ -34,9 +36,12 @@ class Tags(commands.Cog):
 
     @tag.command(name='raw')
     async def raw_tag(self, ctx, tag):
-        data = await guild_get_tag(guildId=ctx.guild.id, key=tag)
-        em = discord.Embed(description=f'```py' f'\n{data}' f'\n```')
-        await ctx.send(embed=em)
+        try:
+            data = await guild_get_tag(guildId=ctx.guild.id, key=tag)
+            em = discord.Embed(description=f'```py' f'\n{data}' f'\n```')
+            await ctx.send(embed=em)
+        except:
+            await ctx.send('Tag not found.')
 
     @tag.command(name='info')
     async def info(self, ctx, tag):
