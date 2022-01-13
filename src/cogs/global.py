5a5d0ccf3b31e14278756bbb0f69a5dc9c69628e
Zen <jnsougata@gmail.com>
2022-01-14 02:02:37 +0530

    removed caching implemented insert

--- src/cogs/global.py
+++ src/cogs/global.py
@@ -11,25 +11,22 @@ class AuxFunc:
     def __init__(self):
         self.deta = Deta(os.getenv('DETA'))
         self.db = self.deta.Base('_PUBLIC_TAGS')
-        self.cache = self.db.get('all')
 
-    async def push_public_tag(self, item: dict, key: str):
-        await self.update_cache(item)
-        return self.db.put({'item': item}, key)
+    async def push_public_tag(self, item: dict, owner_id: int, key: str):
+        return self.db.put({'item': item, 'owner': str(owner_id)}, key)
+
+    async def insert_public_tag(self, item: dict, owner_id: int, key: str):
+        try:
+            self.db.insert({'item': item, 'owner': str(owner_id)}, key)
+            return True
+        except Exception:
+            return False
 
     async def fetch_public_tag(self, key: str):
         data = self.db.get(key)
         if data:
-            return data.get('item')
-        return None
-
-    async def get_cached_tags(self):
-        if self.cache:
-            return self.cache.get('item')
-        return None
-
-    async def update_cache(self, item: dict):
-        self.cache = {'item': item}
+            return data.get('item'), data.get('owner')
+        return None, None
 
 
 class Global(commands.Cog):
@@ -40,91 +37,43 @@ class Global(commands.Cog):
 
     @commands.group(name='gtag', invoke_without_command=True)
     async def tag(self, ctx: commands.Context, tag: str):
-        all_tags = await self.func.get_cached_tags()
-        if all_tags:
-            tag_names = list(all_tags)
-            embed_list = []
-            for name in tag_names:
-                if tag in name:
-                    tag_id = name.replace(f'_', ' ').split(' ')[1]
-                    emd = discord.Embed(
-                        title=f'{all_tags[name]["name"]}',
-                        description=f'{all_tags[name]["content"]}',
-                        color=0xffffff
-                    )
-                    emd.set_footer(text=f'Created by {self.bot.get_user(int(tag_id))} | (id: {tag_id})')
-                    embed_list.append(emd)
-
-            if embed_list:
-                await ctx.send(embeds=embed_list)
-            else:
-                await ctx.send('Tag not found.')
+        data, owner = await self.func.fetch_public_tag(key=tag)
+        if data and owner:
+            emd = discord.Embed(
+                title=f'{data.get("name")}',
+                description=f'{data.get("content")}',
+                color=0xffffff
+            )
+            user = await self.bot.fetch_user(int(owner))
+            emd.set_footer(text=f'Created by {user} | ( id: {owner} )')
+            await ctx.send(embed=emd)
+        else:
+            await ctx.send('Tag not found.')
 
     @tag.command(name='create')
-    async def tag_create(self, ctx: commands.Context, name: str, *, content: str):
+    async def gtag_create(self, ctx: commands.Context, name: str, *, content: str):
         if not len(name) < 3:
             time = datetime.datetime.now()
-            keygen = f'{name}_{ctx.author.id}'
-            all_tags = await self.func.get_cached_tags()
-            if all_tags:
-                previous = all_tags.get(keygen)
-                if previous:
-                    await ctx.send('Tag already exists.')
-                else:
-                    await ctx.send(f'Tag `{name}` successfully created.')
-                    all_tags[keygen] = {
-                        "owner": f'{ctx.author.id}',
-                        "name": name,
-                        "content": content,
-                        "created_at": f'{time.day}/{time.month}/{time.year}'
-                    }
-                    await self.func.push_public_tag(item=all_tags, key='all')
+            data = {
+                "owner": f'{ctx.author.id}',
+                "name": name,
+                "content": content,
+                "created_at": f'{time.day}/{time.month}/{time.year}'
+            }
+            check = await self.func.insert_public_tag(item=data, owner_id=ctx.author.id, key=name)
+            if check:
+                await ctx.send(f'Tag `{name}` created successfully!')
             else:
-                await ctx.send(f'Tag `{name}` successfully created.')
-                item = {
-                    keygen: {
-                        "owner": f'{ctx.author.id}',
-                        "name": name,
-                        "content": content,
-                        "created_at": f'{time.day}/{time.month}/{time.year}'
-                    }
-
-                }
-                await self.func.push_public_tag(item=item, key='all')
+                await ctx.send(f'Tag `{name}` already exists!')
         else:
             await ctx.send('Tag name must be at least 3 characters long.')
 
     @tag.command(name='raw')
-    async def tag_raw(
-            self,
-            ctx: commands.Context,
-            name: str = None,
-            owner: Union[discord.User, discord.Member] = None
-    ):
-        all_tags = await self.func.get_cached_tags()
-        if all_tags:
-            keys = list(all_tags)
-            if name:
-                for key in keys:
-                    if name in key:
-                        await ctx.send(f'```{all_tags[key]}```')
-
-            if owner:
-                for key in keys:
-                    if str(owner.id) in key:
-                        await ctx.send(f'```{all_tags[key]}```')
-
-    @tag.command(name='all')
-    async def tag_raw(self, ctx: commands.Context):
-        all_tags = await self.func.get_cached_tags()
-        string = ''
-        if all_tags:
-            keys = list(all_tags)
-            _replaced = [item.replace('_', ' ') for item in keys]
-            tup_data = [item.split(' ') for item in _replaced]
-            for item in tup_data:
-                string += f'**{item[0]}** (id: {item[1]})\n\n'
-            await ctx.send(embed=discord.Embed(title='All Global Tags', description=f'{string}', color=0xffffff))
+    async def gtag_raw(self, ctx: commands.Context, name: str):
+        data, _ = await self.func.fetch_public_tag(key=name)
+        if data:
+            emd = discord.Embed(description=f'{data}', color=0xffffff)
+            await ctx.send(embed=emd)
 
 
 def setup(bot: discord.Client):
