5964aadc8758b2a8f514d59a3699ef0d7f769f11
Zen <jnsougata@gmail.com>
2022-01-13 12:21:35 +0530

    removed redundant global func renamed camelcase parameters

--- src/cogs/guild.py
+++ src/cogs/guild.py
@@ -12,7 +12,7 @@ class Guild(commands.Cog):
 
     @commands.group(name='tag', invoke_without_command=True)
     async def tag(self, ctx, tag):
-        data = await guild_get_tag(guildId=ctx.guild.id, key=tag)
+        data = await guild_get_tag(guild_id=ctx.guild.id, key=tag)
         if data:
             await ctx.send(data['item'][0]['content'])
         else:
@@ -23,7 +23,7 @@ class Guild(commands.Cog):
         try:
             if 0 < len(name) >= 3:
                 time = datetime.datetime.now()
-                await guild_create_tag(guildId=ctx.guild.id, item=[
+                await guild_create_tag(guild_id=ctx.guild.id, item=[
                     {"owner": f'{ctx.author.id}', "name": name, "content": content,
                      "created_at": f'{time.day}/{time.month}/{time.year}'}], key=name)
                 await ctx.send(f'Tag `{name}` successfully created.')
@@ -36,10 +36,10 @@ class Guild(commands.Cog):
     async def tag_edit(self, ctx, tag, *, content: str):
         time = datetime.datetime.now()
         try:
-            data = await guild_get_tag(guildId=ctx.guild.id, key=tag)
+            data = await guild_get_tag(guild_id=ctx.guild.id, key=tag)
             owner = data['item'][0]['owner']
             if f'{ctx.author.id}' == f'{owner}':
-                await guild_edit_tag(guildId=ctx.guild.id, item=[
+                await guild_edit_tag(guild_id=ctx.guild.id, item=[
                     {"owner": f'{ctx.author.id}', "name": tag, "content": content,
                      "created_at": f'{time.day}/{time.month}/{time.year}'}], key=tag)
                 await ctx.send(f'Tag `{tag}` successfully edited.')
@@ -50,7 +50,7 @@ class Guild(commands.Cog):
 
     @tag.command(name='delete')
     async def tag_delete(self, ctx, tag):
-        data = await guild_get_tag(guildId=ctx.guild.id, key=tag)
+        data = await guild_get_tag(guild_id=ctx.guild.id, key=tag)
         if data and data['item']:
             owner = data['item'][0]['owner']
             view = Confirm(ctx)
@@ -60,7 +60,7 @@ class Guild(commands.Cog):
                 await msg.edit('Tag deletion timed out.')
             elif view.value:
                 if f'{ctx.author.id}' == f'{owner}':
-                    await guild_delete_tag(guildId=ctx.guild.id, key=tag)
+                    await guild_delete_tag(guild_id=ctx.guild.id, key=tag)
                     await msg.edit(f'Tag `{tag}` successfully deleted.', view=None)
                 else:
                     await msg.edit('You don\'t own this tag.', view=None)
@@ -71,14 +71,14 @@ class Guild(commands.Cog):
 
     @tag.command(name='raw')
     async def tag_raw(self, ctx, tag):
-        tag = await guild_get_tag(guildId=ctx.guild.id, key=tag)
+        tag = await guild_get_tag(guild_id=ctx.guild.id, key=tag)
         first_step = discord.utils.escape_markdown(tag['item'][0]['content'])
         await ctx.send(first_step.replace('<', '\\<'))
 
     @tag.command(name='info')
     async def tag_info(self, ctx, tag):
         try:
-            info = await guild_get_tag(guildId=ctx.guild.id, key=tag)
+            info = await guild_get_tag(guild_id=ctx.guild.id, key=tag)
             em = discord.Embed(title=info['item'][0]['name'], colour=0xffffff)
             em.add_field(name='Content', value=info['item'][0]['content'], inline=False)
             em.add_field(name='Owner', value='<@' + info['item'][0]['owner'] + '>', inline=True)
--- src/extras/func.py
+++ src/extras/func.py
@@ -6,37 +6,23 @@ deta = Deta(os.getenv('DETA'))
 
 # Guild
 
-async def guild_create_tag(guildId: int, item: Union[list, dict], key: str):
-    db = deta.Base(f'Guild-{guildId}')
+
+async def guild_create_tag(guild_id: int, item: Union[list, dict], key: str):
+    db = deta.Base(f'Guild-{guild_id}')
     db.insert({'item': item}, key)
 
-async def guild_edit_tag(guildId: int, item: Union[list, dict], key: str):
-    db = deta.Base(f'Guild-{guildId}')
+
+async def guild_edit_tag(guild_id: int, item: Union[list, dict], key: str):
+    db = deta.Base(f'Guild-{guild_id}')
     db.put({'item': item}, key)
 
-async def guild_get_tag(guildId: int, key: str):
-    db = deta.Base(f'Guild-{guildId}')
+
+async def guild_get_tag(guild_id: int, key: str):
+    db = deta.Base(f'Guild-{guild_id}')
     return db.get(key)
 
-async def guild_delete_tag(guildId: int, key: str):
-    db = deta.Base(f'Guild-{guildId}')
-    db.delete(key)
 
-# Global
-
-async def global_create_tag(item: Union[list, dict], key: str):
-    db = deta.Base(f'Global')
-    db.insert({'item': item}, key)
-
-async def global_edit_tag(item: Union[list, dict], key: str):
-    db = deta.Base(f'Global')
-    db.put({'item': item}, key)
-
-async def global_get_tag(key: str):
-    db = deta.Base(f'Global')
-    return db.get(key)
-
-async def global_delete_tag(key: str):
-    db = deta.Base(f'Global')
+async def guild_delete_tag(guild_id: int, key: str):
+    db = deta.Base(f'Guild-{guild_id}')
     db.delete(key)
 
