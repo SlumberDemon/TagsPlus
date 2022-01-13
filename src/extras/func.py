a6ddf54fdfcf76724e1123cc69562294297abccc
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-01-13 14:04:29 +0100

    fetch tags by user

--- src/extras/func.py
+++ src/extras/func.py
@@ -30,6 +30,10 @@ async def guild_delete_tag(guild_id: int, key: str):
 # Test functions
 
 
-def test_guild_create_tag(guild_id: int, item: Union[list, dict], owner: int, key: str):
+def test_guild_create_tag(guild_id: int, item: Union[list, dict], user: int, key: str):
     db = deta.Base(f'Guild-{guild_id}')
-    db.insert({'item': item, 'owner': owner}, key)
+    db.insert({'item': item, 'user': user}, key)
+
+def test_guild_get_user_tags(guild_id: int, user: int):
+    db = deta.Base(f'Guild-{guild_id}')
+    return db.get(user)
