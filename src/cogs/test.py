b2252d50cb4b9a6681d4b35b8445e0c7f3d61615
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-01-13 14:08:12 +0100

    test_tag create

--- src/cogs/test.py
+++ src/cogs/test.py
@@ -16,14 +16,12 @@ class Test(commands.Cog):
         await ctx.send(f'{Emo.Tag} {Emo.Tags} {Emo.TagNotFound} {Emo.TagNeutral} {Emo.TagFound}')
 
     @commands.command(name='tag_test')
-    async def tag_create(self, ctx, name, *, content: str = None):
+    async def test_tag_create(self, ctx, name, *, content: str = None):
 
             if 0 < len(name) >= 3:
                 time = datetime.datetime.now()
                 owner = ctx.author.id
-                await test_guild_create_tag(guild_id=ctx.guild.id, item=[
-                    {"owner": owner, "name": name, "content": content,
-                     "created_at": f'{time.day}/{time.month}/{time.year}'}], user=owner, key=name)
+                await test_guild_create_tag(guild_id=ctx.guild.id, item=[{"owner": owner, "name": name, "content": content, "created_at": f'{time.day}/{time.month}/{time.year}'}], user=owner, key=name)
                 await ctx.send(f'Tag `{name}` successfully created.')
             else:
                 await ctx.send('To `little` characters, please use three or more.')
