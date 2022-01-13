fc557d3ef4be3aaa171085b03c8b480800fc289d
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-01-13 14:00:23 +0100

    temp error sending for easy debugging

--- src/cogs/error.py
+++ src/cogs/error.py
@@ -13,8 +13,10 @@ class Error(commands.Cog):
                 'Please try again with `required` argument(s).',
                 delete_after=10, 
             )
-        elif:
-            await ctx.send(error)
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        await ctx.send(f'```py' f'\n{error}' f'\n```')
 
 def setup(bot):
     bot.add_cog(Error(bot))
