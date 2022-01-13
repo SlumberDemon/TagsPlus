6adb77ba4087b199a5d751fe6eb3f289304d9ab1
Zen <jnsougata@gmail.com>
2022-01-13 12:18:42 +0530

    fixed some codes

--- src/cogs/guild.py
+++ src/cogs/guild.py
@@ -1,7 +1,9 @@
-import discord, datetime
+import discord
+import datetime
 from discord.ext import commands
 from src.extras.func import *
 from src.extras.views import *
+
 
 class Guild(commands.Cog):
 
@@ -17,36 +19,39 @@ class Guild(commands.Cog):
             await ctx.send('Tag not found.')
 
     @tag.command(name='create')
-    async def tag_create(self, ctx, name, *, content:str=None):
+    async def tag_create(self, ctx, name, *, content: str = None):
         try:
-            if 0 < len(name) >=3:
-                time = datetime.datetime.now() 
-                await guild_create_tag(guildId=ctx.guild.id, item=[{"owner":f'{ctx.author.id}', "name":name, "content":content, "created_at":f'{time.day}/{time.month}/{time.year}'}], key=name)
+            if 0 < len(name) >= 3:
+                time = datetime.datetime.now()
+                await guild_create_tag(guildId=ctx.guild.id, item=[
+                    {"owner": f'{ctx.author.id}', "name": name, "content": content,
+                     "created_at": f'{time.day}/{time.month}/{time.year}'}], key=name)
                 await ctx.send(f'Tag `{name}` successfully created.')
             else:
                 await ctx.send('To `little` characters, please use three or more.')
-        except:
-           await ctx.send('This tag already exists.')
+        except Exception:
+            await ctx.send('This tag already exists.')
 
     @tag.command(name='edit')
-    async def tag_edit(self, ctx, tag, *, content:str):
-        time = datetime.datetime.now() 
+    async def tag_edit(self, ctx, tag, *, content: str):
+        time = datetime.datetime.now()
         try:
             data = await guild_get_tag(guildId=ctx.guild.id, key=tag)
             owner = data['item'][0]['owner']
             if f'{ctx.author.id}' == f'{owner}':
-                await guild_edit_tag(guildId=ctx.guild.id, item=[{"owner":f'{ctx.author.id}', "name":tag, "content":content, "created_at":f'{time.day}/{time.month}/{time.year}'}], key=tag)
+                await guild_edit_tag(guildId=ctx.guild.id, item=[
+                    {"owner": f'{ctx.author.id}', "name": tag, "content": content,
+                     "created_at": f'{time.day}/{time.month}/{time.year}'}], key=tag)
                 await ctx.send(f'Tag `{tag}` successfully edited.')
             else:
                 await ctx.send('You don\'t own this tag.', view=None)
-        except:
+        except Exception:
             await ctx.send('Tag not found.')
-
 
     @tag.command(name='delete')
     async def tag_delete(self, ctx, tag):
-        try:
-            data = await guild_get_tag(guildId=ctx.guild.id, key=tag)
+        data = await guild_get_tag(guildId=ctx.guild.id, key=tag)
+        if data and data['item']:
             owner = data['item'][0]['owner']
             view = Confirm(ctx)
             msg = await ctx.send(f'Want to delete `{tag}` tag?', view=view)
@@ -61,8 +66,8 @@ class Guild(commands.Cog):
                     await msg.edit('You don\'t own this tag.', view=None)
             else:
                 await msg.edit('Tag deletion cancelled.', view=None)
-        except:
-            await msg.edit('Tag not found.', view=None)
+        else:
+            await ctx.send('Tag not found.', view=None)
 
     @tag.command(name='raw')
     async def tag_raw(self, ctx, tag):
@@ -79,9 +84,9 @@ class Guild(commands.Cog):
             em.add_field(name='Owner', value='<@' + info['item'][0]['owner'] + '>', inline=True)
             em.add_field(name='Created at', value=info['item'][0]['created_at'], inline=True)
             await ctx.send(embed=em)
-        except:
+        except Exception:
             await ctx.send('Tag not found.')
+
 
 def setup(bot):
     bot.add_cog(Guild(bot))
-    
