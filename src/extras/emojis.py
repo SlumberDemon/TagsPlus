64205dd5521b257970eb9983951c5c8a45915a7d
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-01-13 11:05:52 +0100

    Added emojis

---
+++ src/extras/emojis.py
@@ -0,0 +1,6 @@
+class Emo:
+    Tag = '<:Tag:931120394323251270>'
+    Tags = '<:Tags:931091527936127006>'
+    TagNotFound = '<:TagNotFound:931120394394566666>'
+    TagNeutral = '<:TagNeutral:931120394117726248>'
+    TagFound = '<:TagFound:931120394033823745>'
