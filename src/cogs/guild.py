779c21b1962c823f99f801b4e5581b593adae78c
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-01-14 16:56:21 +0100

    send data

--- src/cogs/guild.py
+++ src/cogs/guild.py
@@ -92,7 +92,7 @@ class Guild(commands.Cog):
     @tag.command(name='search')
     async def tag_search(self, ctx, tag):
         data = await guild_fetch_tag(guild_id=ctx.guild.id, key=tag)
-        await ctx.send(data.items)
+        await ctx.send(data)
 
     @commands.command(name='tags')
     async def user_tags(self, ctx, user: discord.User=None):
