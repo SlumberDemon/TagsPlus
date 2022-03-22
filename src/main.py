a584d9ac17558a93a68c67c612ac822b13d0c620
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-03-22 08:57:58 +0100

    added mention bot as a prefix option

--- src/main.py
+++ src/main.py
@@ -9,15 +9,19 @@ intent = discord.Intents.default()
 intent.members = True
 intent.messages = True
 
-# Setup
+# Prefix
 
+async def custom_prefix(bot, msg):
+    return commands.when_mentioned_or('+')(bot, msg)
+
+# Setup
 
 class Tags(commands.Bot):
 
     __dirs__ = os.listdir('src/cogs')
 
     def __init__(self):
-        super().__init__(intents=intent, command_prefix='+')
+        super().__init__(intents=intent, command_prefix=custom_prefix)
         self.init_ext = ['cogs.' + file[:-3] for file in self.__dirs__ if file.endswith('.py')]
 
     async def on_ready(self):
@@ -27,7 +31,6 @@ class Tags(commands.Bot):
     async def setup_hook(self) -> None:
         for ext in self.init_ext:
             await self.load_extension(ext)
-
 
 tags = Tags()
 
