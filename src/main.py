3b2cf11e444044c9fa3e8e0949ee3f02ec0a04ac
Sofa <72651859+SlumberDemon@users.noreply.github.com>
2022-03-25 10:13:01 +0100

    fix

--- src/main.py
+++ src/main.py
@@ -30,7 +30,7 @@ class Tags(commands.Bot):
     __dirs__ = os.listdir('src/cogs')
 
     def __init__(self):
-        super().__init__(intents=intent, command_prefix=custom_prefix, help_command=TagHelp)
+        super().__init__(intents=intent, command_prefix=custom_prefix, help_command=TagHelp())
         self.init_ext = ['cogs.' + file[:-3] for file in self.__dirs__ if file.endswith('.py')]
 
     async def on_ready(self):
